2f24b0f901d9fadb6cddbe1a3aec68ebc090ee02
Barbara Jeziorek <barbarajeziorek@gmail.com>
2021-02-19 20:23:14 +0100

    kanji modal has vocab examples

--- J-Vocab/src/app/kanji-dialog-box/kanji-dialog-box.component.ts
+++ J-Vocab/src/app/kanji-dialog-box/kanji-dialog-box.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DataBase } from '../data-base';
 import { DialogData } from '../kanji/kanji.component';
 
 @Component({
@@ -8,8 +9,20 @@ import { DialogData } from '../kanji/kanji.component';
   styleUrls: ['./kanji-dialog-box.component.scss']
 })
 export class KanjiDialogBoxComponent implements OnInit {
-
-  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+  vocabularyList = new DataBase().getResources();
+  listToDisplay = [];
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    for (const word of this.vocabularyList) {
+      if (word.character.includes(data.kanji.New)) {
+        this.listToDisplay.push(
+          { 
+            kanji: word.character, 
+            reading: word.sylabs, 
+            meaning: word.meaningPL
+          });
+      }
+    }
+  }
 
   ngOnInit(): void {
   }
